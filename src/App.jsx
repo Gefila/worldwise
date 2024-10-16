2e46ef4e629c4442c6ebcdcdd30178e77ea92e28
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-10-16 18:26:28 +0700

    Implementing Main Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./assets/pages/HomePage";
+import Product from "./assets/pages/Product";
+import Pricing from "./assets/pages/Pricing";
+import PageNotFound from "./assets/pages/PageNotFound";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
