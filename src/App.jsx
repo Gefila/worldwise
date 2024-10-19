fca0cebdb80cab3bea6c74304fb0c8f3d0910c63
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-10-19 08:41:26 +0700

    Using CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./assets/pages/HomePage";
 import Product from "./assets/pages/Product";
 import Pricing from "./assets/pages/Pricing";
 import PageNotFound from "./assets/pages/PageNotFound";
+import AppLayout from "./assets/pages/AppLayout";
 
 function App() {
 	return (
@@ -12,6 +12,7 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
+				<Route path="app" element={<AppLayout />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 export default function HomePage() {
     return (
         <div>
             <PageNav/>
+            <AppNav/>
             <h1>WorldWise</h1>
-            <Link to="/product">Product</Link>
+            <Link to="/app">Go to app</Link>
         </div>
     )
 }
