0f75c9e23be99bd4c9e85f64d9d703bfce826916
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-10-19 09:13:36 +0700

    Building the Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./assets/pages/HomePage";
-import Product from "./assets/pages/Product";
-import Pricing from "./assets/pages/Pricing";
-import PageNotFound from "./assets/pages/PageNotFound";
-import AppLayout from "./assets/pages/AppLayout";
+import HomePage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="app" element={<AppLayout />} />
+				<Route path="login" element={<Login />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
