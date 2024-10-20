acf12f6bd0c298d05c6882209e37e5f00eb0651b
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-10-20 17:25:56 +0700

    Programmatic Navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountriesList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -44,6 +45,7 @@ function App() {
 					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
 					<Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading}/>} />
 					<Route path="cities/:id" element ={<City/>}/>
+					<Route path="form" element ={<Form/>}/>
 				</Route>
 				<Route path="login" element={<Login />} />
 				<Route path="*" element={<PageNotFound />} />
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,19 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 export default function Map() {
+    const navigate = useNavigate();
+
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
     return (
-        <div className={styles.mapContainer}>
+        <div className={styles.mapContainer} onClick={()=> navigate('form')}>
             <h1>MAP</h1>
             <h1>Position: {lat}, {lng}</h1>
+            <button onClick={()=> {
+                setSearchParams({lat: 40, lng: 50})
+            }}>change pos</button>
         </div>
     )
 }
